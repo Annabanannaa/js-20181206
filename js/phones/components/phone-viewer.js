31754a4689c7fa854c6f6d66b5bffdc93b8ed500
Anna <annaa_bananaa@mail.ru>
2019-02-04 15:19:31 +0300

    event back

--- js/phones/components/phone-viewer.js
+++ js/phones/components/phone-viewer.js
@@ -1,49 +1,45 @@
 import Component from '../../component.js';
 
-export default class PhoneViewer  extends Component {
+export default class PhoneViewer extends Component {
 
-    show(phoneDetails){
+    show(phoneDetails) {
         this._phoneDetails = phoneDetails;
 
         super.show();
 
         this._render();
+
+        this._element.addEventListener('click',(event)=>{
+            const button = event.target.closest('[data-element="back-button"]');
+            if (!button){
+                return;
+            }
+            this.emit('back');
+        })
     }
 
     _render() {
         let phone = this._phoneDetails;
-        this._element.innerHTML = `
-            <img class="phone" src=" ${ phone.images[0] }">
 
-            <button>Back</button>
-            <button>Add to basket</button>
+        this._element.innerHTML = `
+      <img class="phone" src="${ phone.images[0] }">
+      <button data-element="back-button">Back</button>
+      <button data-element="add-button">Add to basket</button>
+  
+  
+      <h1>${ phone.name }</h1>
+  
+      <p>${ phone.description }</p>
+  
+      <ul class="phone-thumbs">
+        ${ phone.images.map(imageUrl => `
         
+          <li>
+            <img src="${ imageUrl }">
+          </li>
         
-            <h1>${ phone.name }</h1>
-        
-            <p>${ phone.description }</p>
-        
-            <ul class="phone-thumbs">
-            
-              <li>
-                <img src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
-              </li>
-              <li>
-                <img src="img/phones/motorola-xoom-with-wi-fi.1.jpg">
-              </li>
-              <li>
-                <img src="img/phones/motorola-xoom-with-wi-fi.2.jpg">
-              </li>
-              <li>
-                <img src="img/phones/motorola-xoom-with-wi-fi.3.jpg">
-              </li>
-              <li>
-                <img src="img/phones/motorola-xoom-with-wi-fi.4.jpg">
-              </li>
-              <li>
-                <img src="img/phones/motorola-xoom-with-wi-fi.5.jpg">
-              </li>
-            </ul>
-        `;
+        `).join('') }
+      </ul>
+    `;
     }
 }
